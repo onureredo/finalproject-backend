5cb2893245d6a939eb81372c498c8853d5f54a13
anoj-kt <anojk28@gmail.com>
2022-08-24 22:13:56 +0200

    Added functionality to service_get

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,6 +1,10 @@
+const Service = require('../models/Service')
+
 module.exports.service_get = (req, res) => {
     let serviceId = req.params.serviceId;
-    res.send(serviceId)
+    Service.find({_id: serviceId})
+    .then((result) => res.send(result))
+    .catch((err) => console.log('Error fetchin service: ' + err))
 }
 
 module.exports.newRequest_get = (req, res) => {
