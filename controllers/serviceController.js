d1d4c746cda8ab7a578a8c1951210e2183ff6236
anoj-kt <anojk28@gmail.com>
2022-08-24 20:49:39 +0200

    Added route for POST-new-request

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -7,3 +7,8 @@ module.exports.newRequest_get = (req, res) => {
     let serviceId = req.params.serviceId;
     res.send(serviceId + 'new')
 }
+
+module.exports.newRequest_post = (req, res) => {
+    let serviceId = req.params.serviceId;
+    res.send(serviceId + 'post')
+}
--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -4,6 +4,6 @@ const serviceController = require('../controllers/serviceController')
 
 router.get('/:serviceId', serviceController.service_get);
 router.get('/:serviceId/request/new', serviceController.newRequest_get);
-
+router.post('/:serviceId/request/new', serviceController.newRequest_post);
 
 module.exports = router;
