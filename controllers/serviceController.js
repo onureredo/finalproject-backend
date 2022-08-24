79a13e196d667f000ab4e1069e1fc38c4f2ed488
anoj-kt <anojk28@gmail.com>
2022-08-24 20:47:39 +0200

    Added route for GET-new-request

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -2,3 +2,8 @@ module.exports.service_get = (req, res) => {
     let serviceId = req.params.serviceId;
     res.send(serviceId)
 }
+
+module.exports.newRequest_get = (req, res) => {
+    let serviceId = req.params.serviceId;
+    res.send(serviceId + 'new')
+}
--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const serviceController = require('../controllers/serviceController')
 
 router.get('/:serviceId', serviceController.service_get);
+router.get('/:serviceId/request/new', serviceController.newRequest_get);
 
 
 module.exports = router;
