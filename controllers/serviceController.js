499f68d9e87011a746c0955e8b3cc43924aaa6e4
anoj-kt <anojk28@gmail.com>
2022-08-25 17:12:25 +0200

    Added query to POST-new-request

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,20 +1,27 @@
 const Service = require('../models/Service')
+const Request = require('../models/Request')
 
 module.exports.service_get = (req, res) => {
     let serviceId = req.params.serviceId;
-    Service.find({_id: serviceId})
+    Service.find({_id: serviceId}).populate('serviceProviderId')
     .then((result) => res.send(result))
-    .catch((err) => console.log('Error fetchin service: ' + err))
+    .catch((err) => console.log('Error fetching service: ' + err))
 }
 
 module.exports.newRequest_get = (req, res) => {
     let serviceId = req.params.serviceId;
     Service.find({_id: serviceId}).select('title price currency priceCalculationType')
     .then((result) => res.send(result))
-    .catch((err) => console.log('Error fetchin service: ' + err))
+    .catch((err) => console.log('Error fetching service: ' + err))
 }
 
-module.exports.newRequest_post = (req, res) => {
-    let serviceId = req.params.serviceId;
-    res.send(serviceId + 'post')
-}
+module.exports.newRequest_post = async (req, res) => {
+    const { serviceConsumerId, serviceProviderId, serviceId, effectiveDate, effectiveAddress, status  } = req.body;
+    try {
+        const request = await Request.create({ serviceConsumerId, serviceProviderId, serviceId, effectiveDate, effectiveAddress, status  });
+        res.status(201).json(request)
+    } catch(err) {
+        console.log('Error creating request:' + err);
+        res.status(400).send('Error creating request')
+    }
+}    
