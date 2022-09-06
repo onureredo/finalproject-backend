fc4a7364462ed477b1c6c92d716995481ba86896
anoj-kt <anojk28@gmail.com>
2022-09-06 15:27:35 +0200

    Added routes for creating ad

--- routes/userProvidedServicesRoutes.js
+++ routes/userProvidedServicesRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const userProvidedServicesController = require('../controllers/userProvidedServicesController')
 
-// TODO
-
+router.get('/new', userProvidedServicesController.service_get);
+router.post('/new', userProvidedServicesController.newRequest_get);
 
 module.exports = router;
