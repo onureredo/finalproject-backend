3fe2f2b8cc9de0a606b5e51688d604760fd36005
anoj-kt <anojk28@gmail.com>
2022-09-06 11:35:06 +0200

    Create token for login post

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,10 +93,14 @@ module.exports.login_get = (req, res) => {
 }
 
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
+    const { email, password } = req.body;
+
     try {
-        const { email, password } = req.body;
-        
+        const user = await User.login(email, password)
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: 30 * 60 * 1000 });
+        res.status(200).json({user: user._id})
     } catch (err) {
         res.status(400).send(errors);
     }
