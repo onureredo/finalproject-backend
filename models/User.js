eb625682249ed9702489e10d517bba0153ab3ff1
anoj-kt <anojk28@gmail.com>
2022-09-06 11:14:14 +0200

    Added static login method in user model

--- models/User.js
+++ models/User.js
@@ -135,6 +135,18 @@ userSchema.pre('save', async function (next) {
     next();
 });
 
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email: email})
+    if(user) {
+        const auth = await bcrypt.compare(password, user.password)
+        if(auth) {
+            return user
+        }
+        throw Error('Incorrect password')
+    }
+    throw Error('Incorrect email')
+}
+
 userSchema.plugin(idValidator);
 const User = mongoose.model('User', userSchema);
 module.exports = User;
